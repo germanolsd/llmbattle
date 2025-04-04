f5154cbcaa11bb11fe292046a1e26858498e7d4e
germanolsd <germanolsd@gmail.com>
2025-04-04 13:52:49 -0300

    feat: randomize winner position and update battle animations

--- src/lib/RenderBattle.svelte
+++ src/lib/RenderBattle.svelte
@@ -2,13 +2,14 @@
 
 <script>
   const {winner, loser, reasoning} = $props();
+  let winnerOnLeft = Math.random() < 0.5;
 </script>
 
 <div class="log-entry">
-  <div class="winner" >
+  <div class="winner" class:left={winnerOnLeft} class:right={!winnerOnLeft}>
     {winner.name}
   </div>
-  <div class="loser">
+  <div class="loser" class:left={!winnerOnLeft} class:right={winnerOnLeft}>
     {loser.name}
   </div>
   <div class="reasoning">{reasoning}</div>
@@ -29,7 +30,7 @@
     color: white;
     padding: 10px;
     text-align: center;
-    animation: winnerBattle 2s ease-in-out forwards;
+    animation: winnerBattleLeft 2s ease-in-out forwards;
   }
   .loser {
     grid-area: loser;
@@ -37,8 +38,16 @@
     color: white;
     padding: 10px;
     text-align: center;
-    animation: loserBattle 2s ease-in-out forwards;
+    animation: loserBattleRight 2s ease-in-out forwards;
   }
+  .winner.right {
+    animation: winnerBattleRight 2s ease-in-out forwards;
+  }
+
+  .loser.left {
+    animation: loserBattleLeft 2s ease-in-out forwards;
+  }
+
   .reasoning {
     grid-area: reasoning;
     background-color: #2196F3; /* Blue */
@@ -49,7 +58,7 @@
     transform-origin: 50% 0%;
   }
 
-  @keyframes winnerBattle {
+  @keyframes winnerBattleLeft {
     0% {
       transform: translateX(0);
       background-color: grey;
@@ -114,7 +123,7 @@
       color: white;
     }
   }
-  @keyframes loserBattle {
+  @keyframes loserBattleRight {
     0% {
       transform: translateX(0);
       background-color: grey;
@@ -158,6 +167,137 @@
     }
     70% {
       transform: translateX(-10px);
+      font-size: 1.1em;
+    }
+    75% {
+      transform: translateX(0px);
+      font-size: 1.1em;
+    }
+    80% {
+      transform: translateX(0px) rotateZ(0deg);
+      transform: translateX(0px);
+      font-size: 1.1em;
+    }
+    90% {
+      transform: scale(0.8);
+      background-color: grey;
+    }
+    100% {
+      transform: scale(1);
+      background-color: red;
+      color: white;
+    }
+  }
+
+  @keyframes winnerBattleRight {
+    0% {
+      transform: translateX(0);
+      background-color: grey;
+      color: white;
+      font-size: 1em;
+    }
+    15% {
+      transform: translateX(50px);
+      font-size: 1.1em;
+    }
+    30% {
+      transform: translateX(50px);
+      font-size: 1.1em;
+    }
+    35% {
+      transform: translateX(45px);
+      font-size: 1.1em;
+    }
+    40% {
+      transform: translateX(50px);
+      font-size: 1.1em;
+    }
+    45% {
+      transform: translateX(45px);
+      font-size: 1.1em;
+    }
+    50% {
+      transform: translateX(50px);
+      font-size: 1.1em;
+    }
+    55% {
+      transform: translateX(50px);
+      font-size: 1.1em;
+    }
+    60% {
+      transform: translateX(-10px) rotateZ(-20deg);
+      font-size: 1.1em;
+    }
+    65% {
+      font-size: 1.1em;
+    }
+    70% {
+      transform: translateX(-10px);
+      font-size: 1.1em;
+    }
+    75% {
+      transform: translateX(0px);
+      font-size: 1.1em;
+    }
+    80% {
+      transform: translateX(0px) rotateZ(0deg);
+      transform: translateX(0px);
+      font-size: 1.1em;
+    }
+    90% {
+      transform: scale(1.2);
+      background-color: grey;
+    }
+    100% {
+      transform: scale(1);
+      background-color: green;
+      color: white;
+    }
+  }
+  @keyframes loserBattleLeft {
+    0% {
+      transform: translateX(0);
+      background-color: grey;
+      color: white;
+      font-size: 1em;
+    }
+    15% {
+      transform: translateX(-50px);
+      font-size: 1.1em;
+    }
+    30% {
+      transform: translateX(-50px);
+      font-size: 1.1em;
+    }
+    35% {
+      transform: translateX(-45px);
+      font-size: 1.1em;
+    }
+    40% {
+      transform: translateX(-50px);
+      font-size: 1.1em;
+    }
+    45% {
+      transform: translateX(-55px);
+      font-size: 1.1em;
+    }
+    50% {
+      transform: translateX(-50px);
+      font-size: 1.1em;
+    }
+    55% {
+      transform: translateX(-50px);
+      font-size: 1.1em;
+    }
+    60% {
+      transform: translateX(10px) rotateZ(20deg);
+      font-size: 1.1em;
+    }
+    65% {
+      font-size: 1.1em;
+    }
+    70% {
+      transform: translateX(10px);
       font-size: 1.1em;
     }
     75% {
